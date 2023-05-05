6e4dd4722cbbd695168e567aa4189eec6c54ab4b
Auti <118858934+aolijar@users.noreply.github.com>
2023-05-05 12:40:25 -0500

    Add files via upload

--- src/components/OverallStats.js
+++ src/components/OverallStats.js
@@ -7,28 +7,23 @@ const OverAllStats = ({ filter, data }) => {
   //   const [average, setAverage] = useState(0);
 
   useEffect(() => {
+    // console.log(data, "data");
+
     if (data.length <= 0) return;
     var arr = [];
-    var idx = 0;
     data.map((x) => {
-      if (Number(x.averagePrice) === 0) {
-        idx++;
-        return;
-      }
-
       arr.push(Number(x.averagePrice));
     });
-    setAverage(arr.reduce((a, b) => a + b / (data.length - idx)));
-  }, [data]);
+    setAverage(arr.reduce((a, b) => a + b) / data.length);
+  }, [data.length]);
+
   return (
     <div className={styles.container}>
       <p>
-        Product Location:{" "}
-        {filter.location === "" ? "All California" : filter.location}
+        Product Location: {filter.location === "" ? "All" : filter.location}
       </p>
       <p>
-        Product Type:{" "}
-        {filter.productType === "" ? "All Products" : filter.productType}
+        Product Type: {filter.productType === "" ? "All" : filter.productType}
       </p>
       <p>Average Price: ${average.toFixed(2)}</p>
       <p>Total Items: {data.length}</p>
